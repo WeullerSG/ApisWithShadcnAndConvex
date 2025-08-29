dcb32c0f8f44988faeb68caee26f623e8103a39e
Weuller SG <weullerslack@gmail.com>
2025-08-29 16:07:22 -0300

    Formulario com os dados de endereco

--- convex/model/users/mutation.ts
+++ convex/model/users/mutation.ts
@@ -1,24 +1,32 @@
-import {v} from "convex/values"
+import { v } from "convex/values";
 import { mutation } from "../../_generated/server";
 
-
-export const create = mutation ({
-    args: {
-        name: v.string(),
-        userName: v.string()
-    },
-    handler: async(ctx, args) => {
-        return await ctx.db.insert("users",{
-            name: args.name,
-            userName: args.userName,
-        });
-    }
-})
+export const create = mutation({
+  args: {
+    name: v.string(),
+    userName: v.string(),
+    zipCode:v.optional(v.string()),
+    street:v.optional(v.string()),
+    num: v.optional(v.string()),
+    city:v.optional(v.string()),
+    uf:v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert("users", {
+      name: args.name,
+      userName: args.userName,
+      zipCode: args.zipCode,
+      street: args.street,
+      num: args.num,
+      city: args.city,
+      uf: args.uf,
+    });
+  },
+});
 
 export const remove = mutation({
-    args: {id: v.id("users")},
-    handler: async (ctx, args) => {
-        await ctx.db.delete(args.id);
-    }
-})
-
+  args: { id: v.id("users") },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+  },
+});
